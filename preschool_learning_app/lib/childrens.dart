c0814a78ee053d376319ff92d98da83730f6de4b
Subahari <subaa_2909@hotmail.com>
2021-12-08 12:13:42 +0530

    background image add in tasks tab

--- preschool_learning_app/lib/childrens.dart
+++ preschool_learning_app/lib/childrens.dart
@@ -458,12 +458,12 @@ class _TaskState extends State<Task> {
     //player.play('assets/audio/sample.mp3');
     //player.load('sample.mp3');
     return Container(
-      // decoration: BoxDecoration(
-      //   image: DecorationImage(
-      //     image: AssetImage('assets/background.png'),
-      //     fit: BoxFit.cover,
-      //   ),
-      // ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/task.jfif'),
+          fit: BoxFit.cover,
+        ),
+      ),
       child: GridView(
         gridDelegate:
             SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
