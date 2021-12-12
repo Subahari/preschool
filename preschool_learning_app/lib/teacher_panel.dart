244860c5b121e47b12e5a026a23f764fee5868ea
Subahari <subaa_2909@hotmail.com>
2021-12-12 12:29:18 +0530

    quiz tab bg img add

--- preschool_learning_app/lib/teacher_panel.dart
+++ preschool_learning_app/lib/teacher_panel.dart
@@ -146,12 +146,12 @@ class _UploadQuizState extends State<UploadQuiz> {
     //player.play('assets/audio/sample.mp3');
     //player.load('sample.mp3');
     return Container(
-      // decoration: BoxDecoration(
-      //   image: DecorationImage(
-      //     image: AssetImage('assets/background.png'),
-      //     fit: BoxFit.cover,
-      //   ),
-      // ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/Top_new.webp'),
+          fit: BoxFit.cover,
+        ),
+      ),
       child: GridView(
         gridDelegate:
             SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
